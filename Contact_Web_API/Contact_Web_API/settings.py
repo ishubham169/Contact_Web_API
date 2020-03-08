4c61052badc82d71c793ae17323e19f7ebb5fba5
Shubham Kaushik <shubham.kaushik@1mg.com>
2020-03-08 14:03:54 +0530

    cors check added

--- Contact_Web_API/Contact_Web_API/settings.py
+++ Contact_Web_API/Contact_Web_API/settings.py
@@ -42,17 +42,22 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'API',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = ["http://localhost:3001",]
+
 
 ROOT_URLCONF = 'Contact_Web_API.urls'
 
